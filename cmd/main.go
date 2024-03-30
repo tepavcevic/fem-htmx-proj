105a07fc9062f390e06cc7d519b2010a29ade268
tepavcevic <djordje.tepa@gmail.com>
2024-03-30 15:47:55 +0100

    wrapping up the workshop

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -23,8 +27,75 @@ func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) e
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	ID    string
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		ID:    generateUUID(),
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d Data) indexOf(id string) int {
+	for i, contact := range d.Contacts {
+		if contact.ID == id {
+			return i
+		}
+	}
+	return -1
+}
+
+func (d Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("Djordje", "djordje.tepa@gmail.com"),
+			newContact("Clara", "cdoe@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
@@ -32,18 +103,62 @@ func main() {
 	e.Renderer = NewTemplates()
 	e.Use(middleware.Logger())
 
-	count := Count{
-		Count: 0,
-	}
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
+	page := newPage()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index", count)
+		return c.Render(http.StatusOK, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(http.StatusOK, "index", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(http.StatusUnprocessableEntity, "form", formData)
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		if err := c.Render(http.StatusOK, "form", newFormData()); err != nil {
+			log.Fatal(err)
+		}
+		return c.Render(http.StatusOK, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(time.Second * 3)
+		id := c.Param("id")
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(http.StatusNotFound, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(http.StatusOK)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
+}
+
+func generateUUID() string {
+	uuid := make([]byte, 16)
+	_, err := rand.Read(uuid)
+	if err != nil {
+		panic(err)
+	}
+	uuid[6] = (uuid[6] & 0x0f) | 0x40
+	uuid[8] = (uuid[8] & 0x3f) | 0x80
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
 }
